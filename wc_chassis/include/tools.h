9697529d6fc6188f70606047a92a0a4112bae03b
zimuxingjun <lz@gs-robot.com>
2016-04-13 16:32:49 +0800

    ad

--- wc_chassis/include/tools.h
+++ wc_chassis/include/tools.h
@@ -1,4 +1,7 @@
 #ifndef __TOOLS__
 #define __TOOLS__
 
+extern unsigned int remote_ret_;
+extern unsigned int remote_ret_cnt_;
+
 #endif
--- wc_chassis/src/tools.cpp
+++ wc_chassis/src/tools.cpp
@@ -1,2 +1,3 @@
 #include "tools.h"
-
+ unsigned int remote_ret_ = 0x0a00;
+ unsigned int remote_ret_cnt_ = 0;
