7b5973fe8955c71a0b23565658803725ed3cdb27
caomunian <1026989012@qq.com>
2016-04-13 16:50:38 +0800

    add

--- wc_chassis/include/tools.h
+++ wc_chassis/include/tools.h
@@ -1,6 +1,32 @@
 #ifndef __TOOLS__
 #define __TOOLS__
 #include "wc_chassis_mcu.h"  // NOLINT
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Imu.h>
+#include <std_msgs/UInt32.h>
+#include <std_msgs/Float32.h>
+#include <tf/message_filter.h>
+#include <tf/transform_broadcaster.h>
+#include <diagnostic_msgs/DiagnosticStatus.h>
+#include <diagnostic_msgs/KeyValue.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/Range.h>
+#include <autoscrubber_services/StopScrubber.h>
+#include <autoscrubber_services/StartRotate.h>
+#include <autoscrubber_services/StopRotate.h>
+#include <autoscrubber_services/CheckRotate.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <sched.h>  // for setting priority
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 extern unsigned int remote_ret_;
 extern unsigned int remote_ret_cnt_;
 
