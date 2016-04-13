2da64c7c1f88bb0496b3a557b2ce3be13de715d9
caomunian <1026989012@qq.com>
2016-04-13 22:33:38 +0800

    add

--- wc_chassis/include/parameter.h
+++ wc_chassis/include/parameter.h
@@ -1,6 +1,31 @@
 #ifndef __PARAMETER__
 #define __PARAMETER__
-
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Imu.h>
+#include <std_msgs/UInt32.h>
+#include <std_msgs/Float32.h>
+#include <tf/message_filter.h>
+#include <tf/transform_broadcaster.h>
+#include <diagnostic_msgs/DiagnosticStatus.h>
+#include <diagnostic_msgs/KeyValue.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/Range.h>
+#include <autoscrubber_services/StopScrubber.h>
+#include <autoscrubber_services/StartRotate.h>
+#include <autoscrubber_services/StopRotate.h>
+#include <autoscrubber_services/CheckRotate.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <sched.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 extern  double ACC_LIM_TH ;
 extern  double ultral_effective_range ;
 extern  double g_odom_x   ;
