9f871528e17bb92358a3fd289a1da67a05482e4e
caomunian <1026989012@qq.com>
2016-04-13 20:45:23 +0800

    add

--- wc_chassis/src/advertise_service.cpp
+++ wc_chassis/src/advertise_service.cpp
@@ -1,6 +1,8 @@
 #include"advertise_service.h"
+#include"init.h"
 
-bool StartRotate(autoscrubber_services::StartRotate::Request& req, autoscrubber_services::StartRotate::Response& res) {
+bool StartRotate(autoscrubber_services::StartRotate::Request& req,
+                 autoscrubber_services::StartRotate::Response& res) {
   rotate_angle = req.rotateAngle.data;
   start_rotate_flag = true;
   is_rotate_finished = false;
@@ -9,13 +11,15 @@ bool StartRotate(autoscrubber_services::StartRotate::Request& req, autoscrubber_
 }
 
 
-bool StopRotate(autoscrubber_services::StopRotate::Request& req, autoscrubber_services::StopRotate::Response& res) {
+bool StopRotate(autoscrubber_services::StopRotate::Request& req,
+                autoscrubber_services::StopRotate::Response& res) {
   start_rotate_flag = false;
   return true;
 }
 
 
-bool CheckRotate(autoscrubber_services::CheckRotate::Request& req, autoscrubber_services::CheckRotate::Response& res) {
+bool CheckRotate(autoscrubber_services::CheckRotate::Request& req,
+                 autoscrubber_services::CheckRotate::Response& res) {
   res.isFinished.data = is_rotate_finished;
   return true;
 }
