316c074f6604aec5b3b88bb1b29c42ad9442abcd
caomunian <1026989012@qq.com>
2016-04-13 15:32:09 +0800

    ad

--- wc_chassis/src/main.cpp
+++ wc_chassis/src/main.cpp
@@ -1,5 +1,32 @@
 /* Copyright(C) Gaussian Robot. All rights reserved.
 */
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Imu.h>
+#include <std_msgs/UInt32.h>
+#include <std_msgs/Float32.h>
+#include <tf/message_filter.h>
+#include <tf/transform_broadcaster.h>
+#include <diagnostic_msgs/DiagnosticStatus.h>
+#include <diagnostic_msgs/KeyValue.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/Range.h>
+#include <autoscrubber_services/StopScrubber.h>
+#include <autoscrubber_services/StartRotate.h>
+#include <autoscrubber_services/StopRotate.h>
+#include <autoscrubber_services/CheckRotate.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <sched.h>  // for setting priority
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include "wc_chassis_mcu.h"  // NOLINT
 #include "tools.h"
 
 WC_chassis_mcu g_chassis_mcu;
