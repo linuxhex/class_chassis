f66dd3b2692dc4596e79d06f01446efec8a3408a
caomunian <1026989012@qq.com>
2016-04-13 18:05:44 +0800

    add

--- wc_chassis/src/main.cpp
+++ wc_chassis/src/main.cpp
@@ -42,12 +42,12 @@ int main(int argc, char **argv) {
  ros::Publisher gyro_pub  = p_device_nh->advertise<sensor_msgs::Imu>("gyro", 50);
  ros::Publisher remote_cmd_pub  = p_device_nh->advertise<std_msgs::UInt32>("remote_cmd", 50);
  ros::Publisher device_pub = p_device_nh->advertise<diagnostic_msgs::DiagnosticStatus>("device_status", 50);
- ros::Publisher ultrasonic_pubs[0] = p_n->advertise<sensor_msgs::Range>("ultrasonic0", 50);
- ros::Publisher ultrasonic_pubs[1] = p_n->advertise<sensor_msgs::Range>("ultrasonic1", 50);
- ros::Publisher ultrasonic_pubs[2] = p_n->advertise<sensor_msgs::Range>("ultrasonic2", 50);
- ros::Publisher ultrasonic_pubs[3] = p_n->advertise<sensor_msgs::Range>("ultrasonic3", 50);
- ros::Publisher ultrasonic_pubs[4] = p_n->advertise<sensor_msgs::Range>("ultrasonic4", 50);
- ros::Publisher ultrasonic_pubs[5] = p_n->advertise<sensor_msgs::Range>("ultrasonic5", 50);
+ ultrasonic_pubs[0] = p_n->advertise<sensor_msgs::Range>("ultrasonic0", 50);
+ ultrasonic_pubs[1] = p_n->advertise<sensor_msgs::Range>("ultrasonic1", 50);
+ ultrasonic_pubs[2] = p_n->advertise<sensor_msgs::Range>("ultrasonic2", 50);
+ ultrasonic_pubs[3] = p_n->advertise<sensor_msgs::Range>("ultrasonic3", 50);
+ ultrasonic_pubs[4] = p_n->advertise<sensor_msgs::Range>("ultrasonic4", 50);
+ ultrasonic_pubs[5] = p_n->advertise<sensor_msgs::Range>("ultrasonic5", 50);
 
   while (ros::ok()) {
     g_chassis_mcu->getOdo(g_odom_x, g_odom_y, g_odom_tha);
